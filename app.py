9a3c0cddc86b4ef473141e865229f7fd553595be
Taha Zaheer <goldkutch@gmail.com>
2025-08-04 17:25:30 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(1000),  nullable = False)
+    date = db.Column(db.DateTime, default = datetime.now())
+
+    def __repr__(self):
+        return f" Sno: {self.sno} \n Title: {self.title}"
+
+@app.route('/')
+def hello_world():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
